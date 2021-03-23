c81e9da50d9b2f8778af2c197132d903ab11e336
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-03-23 15:54:54 +0530

    [MR.8.4] Getting Data from server

--- src/App.js
+++ src/App.js
@@ -1,25 +1,71 @@
-import './App.css';
+import { Component } from "react"
+import axios from "axios"
+import "./App.css"
 
-function App() {
-  return (
-    <>
-        <button className="btn btn-primary my-2">
-          Add
-        </button>
-        <table className="table">
-          <thead>
-            <tr>
-              <th>Title</th>
-              <th>Update</th>
-              <th>Delete</th>
-            </tr>
-          </thead>
-          <tbody>
-            
-          </tbody>
-        </table>
-      </>
-  );
+export default class App extends Component {
+	state = {
+		posts: [],
+	}
+
+	async componentDidMount() {
+		// when we send a http-request, there's a little delay of 5-10ms or 1s or longer until we get the result
+		// the promise object promises to hold the result of an asynchronus operation.
+		// when we create a promise, initially it's in "pending" state. It'll change in "resolved/fulfilled (in case of success)" or "rejected (in case of faliure)"
+		const { data: posts } = await axios.get(
+			"https://jsonplaceholder.typicode.com/posts"
+		)
+		this.setState({ posts })
+	}
+
+	handleAdd = () => {
+		console.log("Add")
+	}
+
+	handleUpdate = (post) => {
+		console.log("Update", post)
+	}
+
+	handleDelete = (post) => {
+		console.log("Delete", post)
+	}
+
+	render() {
+		return (
+			<>
+				<button className="btn btn-primary my-2">Add</button>
+				<table className="table">
+					<thead>
+						<tr>
+							<th>Title</th>
+							<th>Update</th>
+							<th>Delete</th>
+						</tr>
+					</thead>
+					<tbody>
+						{this.state.posts.map((post) => (
+							<tr key={post.id}>
+								<td>{post.title}</td>
+								<td>
+									<button
+										className="btn btn-info btn-sm"
+										onClick={() => this.handleUpdate(post)}
+									>
+										Update
+									</button>
+								</td>
+								<td>
+									<button
+										className="btn btn-danger btn-sm"
+										onClick={() => this.handleDelete(post)}
+									>
+										Delete
+									</button>
+								</td>
+							</tr>
+						))}
+					</tbody>
+				</table>
+			</>
+		)
+	}
 }
-
-export default App;
