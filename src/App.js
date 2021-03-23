4073acaf86e918ca937fe655149055172381a5ca
Soumyadeep Mondal <soumyadeepmondal1234@gmail.com>
2021-03-23 16:18:12 +0530

    [MR.8.5] Creating Data.

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import { Component } from "react"
 import axios from "axios"
 import "./App.css"
 
+const API_ENDPOINT = "https://jsonplaceholder.typicode.com/posts"
+
 export default class App extends Component {
 	state = {
 		posts: [],
@@ -11,14 +13,23 @@ export default class App extends Component {
 		// when we send a http-request, there's a little delay of 5-10ms or 1s or longer until we get the result
 		// the promise object promises to hold the result of an asynchronus operation.
 		// when we create a promise, initially it's in "pending" state. It'll change in "resolved/fulfilled (in case of success)" or "rejected (in case of faliure)"
-		const { data: posts } = await axios.get(
-			"https://jsonplaceholder.typicode.com/posts"
-		)
+		const { data: posts } = await axios.get(API_ENDPOINT)
 		this.setState({ posts })
 	}
 
-	handleAdd = () => {
-		console.log("Add")
+	handleAdd = async () => {
+		const obj = { title: "a", body: "b" }
+
+		// Because we're creating data, we need to include this data in the body of the request. So, we need to set this object (obj) to the server. So, we'll pass obj as a 2nd argument in post() method.
+
+		// In this case, when we send a http-request, to create a new post the server or the backend will respond with the newly created post (Not the entire list of posts)
+		const { data: post } = await axios.post(API_ENDPOINT, obj)
+
+		// Create the state
+		const posts = [post, ...this.state.posts]
+
+		// Update the state
+		this.setState({ posts })
 	}
 
 	handleUpdate = (post) => {
@@ -32,7 +43,9 @@ export default class App extends Component {
 	render() {
 		return (
 			<>
-				<button className="btn btn-primary my-2">Add</button>
+				<button className="btn btn-primary my-2" onClick={this.handleAdd}>
+					Add
+				</button>
 				<table className="table">
 					<thead>
 						<tr>
